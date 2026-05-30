6286b4f9ef0371797613d71f54584245fb17600f
MrAkash <sky42161@gmail.com>
2026-05-30 13:18:38 +0530

    Fix streamlit file path in start.sh

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ python consumer/consumer.py &
 uvicorn backend.main:app --host=127.0.0.1 --port=8000 &
 
 # 3. Start Streamlit using explicit assignments to prevent parameter shifts
-streamlit run frontend/app.py --server.port=${PORT:-8501} --server.address=0.0.0.0
+streamlit run frontend/streamlit_app.py --server.port=${PORT:-8501} --server.address=0.0.0.0
